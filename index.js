ff6d1bce25ebbf322ab4ef153e31972b0398f8be
Justin Choi <justin@Justins-MacBook-Pro-2.local>
2025-07-12 12:13:29 -0400

    Fetching Weather

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ require('dotenv').config();
 const twilio = require('twilio');
 require('dotenv').config();
 const axios = require('axios');
-const API_KEY = process.env.MARTA_API_KEY;
+const { ReassignedNumberRequest } = require('twilio/lib/rest/lookups/v2/query');
+const M_API_KEY = process.env.MARTA_API_KEY;
+const W_API_KEY = process.env.WEATHER_API_KEY;
 
-const fetchMarta = async () => {
+const fetchMarta = async (weather) => {
     try {
-        const response = await axios.get(`https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/developerservices/traindata?apiKey=${API_KEY}`);
+        const response = await axios.get(`https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/developerservices/traindata?apiKey=${M_API_KEY}`);
         const posts = response.data;
         const arrival = "MIDTOWN STATION";
         const trainArrivalData = [];
-        const printList = [];
 
+        console.log(posts)
         for (let i = 0; i < posts.length; i++) {
 
             if (posts[i].WAITING_TIME == "Arriving") {
@@ -42,27 +44,57 @@ const fetchMarta = async () => {
                 + nextArrival+ "PM).";
             }
         }
+        printString += " The weather will be " + weather + " today.";
         console.log(printString);
 
+        return printString;
 
     } catch (err) {
       console.error('Error fetching posts:', err.message);
     }
 };
 
-const sendWhatsApp = async () => {
+const sendWhatsApp = async (text) => {
     const accountSid = process.env.ACCOUNTSID;
     const authToken = process.env.AUTHTOKEN;
+    const phoneNumber = process.env.PHONENUMBER;
+    const contentSid = process.env.CONTENTSID;
     const client = require('twilio')(accountSid, authToken);
-
+    console.log(text);
     client.messages
     .create({
         from: 'whatsapp:+14155238886',
-        contentSid: 'HXb5b62575e6e4ff6129ad7c8efe1f983e',
-        contentVariables: '{"1":"12/1","2":"3pm"}',
-        to: 'whatsapp:+14849053260'
+        contentSid: contentSid,
+        contentVariables: `{"1":"${text}"}`,
+        to: phoneNumber
     })
 
 }
-fetchMarta();
-sendWhatsApp();
+
+fetchWeather = async () => {
+    try {
+
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${W_API_KEY}&q=33.781738,-84.383018`);
+        posts = response.data;
+
+        // console.log(posts);
+
+        weather = posts.current.condition.text;
+        console.log(weather);
+
+
+        return weather;
+
+    } catch {
+        console.log("Failed");
+    }
+}
+
+const run = async () => {
+    weather = await fetchWeather();
+    message = await fetchMarta(weather);
+    // // console.log(message);
+    sendWhatsApp(message);
+}
+
+run();
